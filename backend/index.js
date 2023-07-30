4abdba54929e910956937372be51774f18711938
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2023-07-30 15:44:19 +0300

    Add cors

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const user = require('./routes/user')
 require('./mongo-connection')
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 app.use('/user', user)
 
 app.get('/', (req, res) => {
