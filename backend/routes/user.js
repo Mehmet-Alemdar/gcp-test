575688a2527680d174f8491e941c2cf30eaf30a1
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2023-07-30 15:21:21 +0300

    Add status code to apis

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,34 +2,47 @@ const router = require('express').Router();
 const User =require('../models/user');
 const userService = require('../services/userService');
 
-
-
-
 router.get('/:adress', async (req, res) => {
   const { adress } = req.params
 
-  const users = await userService.getUsersByAdress(adress)
-  res.send(users)
+  try {
+    const users = await userService.getUsersByAdress(adress)
+    res.status(200).send(users)
+  } catch (error) {
+    res.status(500).send(error)
+  }
 })
 
 router.get('/', async (req, res) => {
-  const users =  await userService.getUsers()
-  res.send(users)
+  try {
+    const users =  await userService.getUsers()
+    res.status(200).send(users)
+  } catch (error) {
+    res.status(500).send(error)
+  }
 });
 
 router.post('/', (req, res) => {
   const obj = req.body
 
-  const user = new User(obj)
-  userService.create(user)
-  res.send(user)
+  try {
+    const user = new User(obj)
+    userService.create(user)
+
+    res.status(200).send(user)
+  } catch (error) {
+    res.status(500).send(error)
+  }
 });
 
 router.delete('/:id', async (req, res) => {
   const { id } = req.params
-
-  const user = await userService.deleteUserById(id)
-  res.send(user)
+  try {
+    const user = await userService.deleteUserById(id)
+    res.status(200).send(user)
+  } catch (error) {
+    res.status(500).send(error)
+  }
 })
 
 module.exports = router;
