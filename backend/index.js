47dacca6e7b412297d0c4a9dd586cbd76f6df4f9
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2023-08-03 15:18:20 +0300

    Update port in backend

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,8 @@ app.get('/', (req, res) => {
   res.send('Hi')
 })
 
-app.listen(5000, () => {
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
   console.log('Server is running on port 5000')
 })
